691370e228e32f31fa43256393fe432b2ae06d43
vinpuch <94136786+vinpuch@users.noreply.github.com>
2022-10-28 09:32:36 +0200

    Start

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,69 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+
+import static org.example.Main.Gender.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("John", MALE),
+                new Person("Alice", FEMALE)
+
+
+        );
+
+        System.out.println("// Imperative approach");
+        // Imperative approach
+
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        // Declareitave approach
+        System.out.println("// Declareitave approach");
+
+        List<Person> females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
     }
 }
